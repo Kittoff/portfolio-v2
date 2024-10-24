9e2e46df949a0429750a17c517ab6c4d472c3a78
Kittoff <lozano.christophe@protonmail.com>
2024-10-24 22:26:37 +0200

    removed md:800px for full screen

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -4,7 +4,7 @@ import Content from "./Content";
 export default function Footer() {
   return (
     <div
-      className="relative md:h-[800px]"
+      className="relative h-screen"
       style={{ clipPath: "polygon(0% 0, 100% 0%, 100% 100%, 0 100%)" }}
     >
       <div className="md:fixed bottom-0 h-screen w-full">
