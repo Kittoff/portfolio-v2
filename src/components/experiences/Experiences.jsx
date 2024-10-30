e392b04eeac26897967547031c8cefc73d279c3a
Kittoff <lozano.christophe@protonmail.com>
2024-10-30 16:33:21 +0100

    cleaning

--- src/components/experiences/Experiences.jsx
+++ src/components/experiences/Experiences.jsx
@@ -1,13 +1,24 @@
-import React from 'react';
+import React from "react";
 
-const ContentSection = ({ bgColor, offset, imgSrc, title, text, id,subtitle }) => (
-  <div className={`h-screen sticky top-0 flex flex-col  content-center justify-center items-center ${bgColor}`} style={{ top: offset }}>
+const ContentSection = ({
+  bgColor,
+  offset,
+  imgSrc,
+  title,
+  text,
+  id,
+  subtitle,
+}) => (
+  <div
+    className={`sticky top-0 flex h-screen flex-col content-center items-center justify-center ${bgColor}`}
+    style={{ top: offset }}
+  >
     {/* <img className="w-[160%] justify-self-end" src={imgSrc} alt={title} /> */}
-    <h2 className="text-[clamp(2rem,6vw,5rem)] uppercase leading-none font-normal tracking-tighter max-w-[500px]">
-      <sup>{id}</sup> {title} 
+    <h2 className="max-w-[500px] text-[clamp(2rem,6vw,5rem)] font-normal uppercase leading-none tracking-tighter">
+      <sup>{id}</sup> {title}
     </h2>
-    <h4 className='text-center text-step_p__1' >{subtitle}</h4>
-    <p className="text-left text-step_p_3 leading-normal p-6 backface-hidden">
+    <h4 className="text-center text-step_p__1">{subtitle}</h4>
+    <p className="backface-hidden max-w-[950px] p-6 text-left text-step_p_3 leading-normal">
       {text}
     </p>
   </div>
@@ -17,55 +28,55 @@ const Experience = () => {
   const sections = [
     {
       bgColor: "bg-[#dfd6cf]",
-      id:"01",
+      id: "01",
       offset: "0rem",
       imgSrc: "img/1.png",
       title: "XDog",
-      subtitle:"Serveur Multi Mission Coverage Management pour Pléiades / Astroterra Et les futurs satellites d’observation de la terre.",
-      text: "Conceptualiser et intégrer une interface afin de récupérer des données d'images satellites dans une zone définie par l'utilisateur."
+      subtitle:
+        "Serveur Multi Mission Coverage Management pour Pléiades / Astroterra Et les futurs satellites d’observation de la terre.",
+      text: "Conceptualiser et intégrer une interface afin de récupérer des données d'images satellites dans une zone définie par l'utilisateur.",
     },
     {
       bgColor: "bg-[#c5c3c0]",
-      id:"02",
+      id: "02",
       offset: "2rem",
       imgSrc: "img/2.png",
       title: "Walk",
-      subtitle:"gestion de maintenance moteur Safran",
-      text: "Conceptualiser et intégrer un outil de maintenance de moteurs d'avions à destination des techniciens."
+      subtitle: "gestion de maintenance moteur Safran",
+      text: "Conceptualiser et intégrer un outil de maintenance de moteurs d'avions à destination des techniciens.",
     },
     {
       bgColor: "bg-[#dfd6cf]",
-      id:"03",
+      id: "03",
       offset: "4rem",
       imgSrc: "img/3.png",
       title: "MyCM",
-      subtitle:"Gestion humaine",
-      text: "Une grande entreprise a besoin d'outil afin de gérer au mieux les relations humaines entre collaborateurs. je me suis occupé de l'intégration de la logique métier à destination des services RH."
+      subtitle: "Gestion humaine",
+      text: "Une grande entreprise a besoin d'outil afin de gérer au mieux les relations humaines entre collaborateurs. je me suis occupé de l'intégration de la logique métier à destination des services RH.",
     },
     {
       bgColor: "bg-[#c5c3c0]",
-      id:"04",
+      id: "04",
       offset: "6rem",
       imgSrc: "img/4.png",
       title: "Skywise",
-      subtitle:"gestion de flote aéronautique",
-      text: "Intégration de nouvelles interfaces à partie de maquettes UI/UX à destination des compagnie aériennes afin de gérer au mieux la maintenance de leurs avions."
+      subtitle: "gestion de flote aéronautique",
+      text: "Intégration de nouvelles interfaces à partie de maquettes UI/UX à destination des compagnie aériennes afin de gérer au mieux la maintenance de leurs avions.",
     },
     {
       bgColor: "bg-[#dfd6cf]",
-      id:"05",
+      id: "05",
       offset: "8rem",
       imgSrc: "img/5.png",
       title: "Mars2020",
-      subtitle:"interface utilisateur pour mission Perseverance sur Mars",
-      text: "Implémentation de nouvelles interfaces à destination des scientifiques du CNES et de la NASA afin d'envoyer des ordres de missions au robot Perseverance, présent sur Mars."
-    }
+      subtitle: "interface utilisateur pour mission Perseverance sur Mars",
+      text: "Implémentation de nouvelles interfaces à destination des scientifiques du CNES et de la NASA afin d'envoyer des ordres de missions au robot Perseverance, présent sur Mars.",
+    },
   ];
 
   return (
     <div className="wrap bg-red-300">
       {sections.map((section, index) => (
-
         <ContentSection key={index} {...section} />
       ))}
     </div>
