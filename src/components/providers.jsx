4416242a9e80189df35559d3f250615d56a6b9f8
Kittoff <lozano.christophe@protonmail.com>
2024-11-27 11:01:53 +0100

    loader transition

--- src/components/providers.jsx
+++ src/components/providers.jsx
@@ -1,9 +1,9 @@
 "use client";
-
 import { useRef } from "react";
 import { gsap } from "gsap";
 import { TransitionRouter } from "next-transition-router";
 import { useMenuContext } from "@/utils/MenuContext";
+import Loader from "@/utils/loader/Loader";
 
 export function Providers({ children }) {
   const firstLayer = useRef(null);
@@ -76,12 +76,17 @@ export function Providers({ children }) {
 
       <div
         ref={firstLayer}
-        className="fixed inset-0 z-50 translate-y-full bg-[#69a4b4]"
-      />
+        className="fixed inset-0 z-50 flex translate-y-full items-center justify-center bg-[#69a4b4]"
+      >
+        <Loader />
+      </div>
+
       <div
         ref={secondLayer}
-        className="fixed inset-0 z-50 translate-y-full bg-[#5a729e]"
-      />
+        className="fixed inset-0 z-50 flex translate-y-full items-center justify-center bg-[#5a729e]"
+      >
+        <Loader />
+      </div>
     </TransitionRouter>
   );
 }
