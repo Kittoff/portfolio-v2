9b64a7580778a136186fae824bbf686d7d004b30
Kittoff <lozano.christophe@protonmail.com>
2024-11-28 15:46:44 +0100

    font preload

--- src/app/[locale]/layout.jsx
+++ src/app/[locale]/layout.jsx
@@ -10,18 +10,26 @@ import TranslationsProvider from "@/components/TranslationProvider";
 import initTranslations from "../i18n";
 
 const bigilla = localFont({
-  src: "./fonts/Bigilla.otf",
+  src: [
+    {
+      path: "./fonts/Bigilla.otf",
+      weight: "normal",
+      style: "normal",
+    },
+    {
+      path: "./fonts/Bigilla-Bold.otf",
+      weight: "normal",
+      style: "bold",
+    },
+  ],
   variable: "--font-bigilla",
+  preload: true,
   weight: "100 900",
 });
-// const geistMono = localFont({
-//   src: "./fonts/GeistMonoVF.woff",
-//   variable: "--font-geist-mono",
-//   weight: "100 900",
-// });
 
 export const melodrama = localFont({
   src: "./fonts/MelodramaVF.woff",
+  preload: true,
   variable: "--font-melodrama",
   weight: "300 700",
 });
@@ -29,40 +37,38 @@ export const melodrama = localFont({
 const inter = Inter({
   subsets: ["latin"],
   variable: "--font-inter",
+  preload: true,
   display: "swap",
 });
 
-export async function generateMetadata({ params: { locale } }) {
-  const { t } = await initTranslations(locale, ["metadata"]);
-
-  return {
-    metadataBase: new URL(process.env.NEXT_PUBLIC_BASE_URL),
-    title: {
-      default: "Christophe Lozano • Freelance Developer",
-      // template,
-    },
-    openGraph: {
-      description: t("metadata_description"),
-      images: ["/ogImage.png"],
-    },
-    keywords: [
-      "developer",
-      "développeur",
-      "website",
-      "Freelance Web Developer",
-      "Creative Web Development",
-      "Website Creation",
-      "Full-Stack Developer",
-      "Custom Web Solutions",
-      "JavaScript Expert",
-      "Web Development Consultant",
-      "Freelance Developer Portfolio",
-      "SEO Optimization for Websites",
-      "Front-End Development",
-      "Creative Web Design and Development",
-    ],
-  };
-}
+export const metadata = {
+  metadataBase: new URL(process.env.NEXT_PUBLIC_BASE_URL),
+  title: {
+    default: "Christophe Lozano • Freelance Developer",
+    // template,
+  },
+  openGraph: {
+    description:
+      "Christophe Lozano est un développeur qui se spécialise dans l'animation et interraction. En tant qu'indépendant, il travaille avec des entreprises, agences, startups et particuliers à travers le monde.",
+    images: ["/ogImage.png"],
+  },
+  keywords: [
+    "developer",
+    "développeur",
+    "website",
+    "Freelance Web Developer",
+    "Creative Web Development",
+    "Website Creation",
+    "Full-Stack Developer",
+    "Custom Web Solutions",
+    "JavaScript Expert",
+    "Web Development Consultant",
+    "Freelance Developer Portfolio",
+    "SEO Optimization for Websites",
+    "Front-End Development",
+    "Creative Web Design and Development",
+  ],
+};
 
 export function generateStaticParams() {
   return i18nConfig.locales.map((locale) => ({ locale }));
